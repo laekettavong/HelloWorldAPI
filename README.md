23bf78330265e3e56241491021be781dd7c55f7f
Khoutsaneh Kettavong <laekettavong@Khoutsanehs-iMac.local>
2018-09-11 11:55:57 -0400

    Refactored to run across all cores of host machine

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # HelloWorldAPI
-Vanilla node.js RESTful API that returns a simple JSON
+Simple 'Hello World' RESTful API written with raw Node (no third-party modules). The app is set up to run across all cores on the host machine using the native 'os' and 'cluster' modules.
+
+
+### Say Hello
+Endpoint: `<host>`:`<port>`/hello  
+Method: POST/PUT 
+Request body: arbitrary payload must be in JSON format 
+Required fields : none  
+  
+Sample request
+
+```
+{
+	"firstName":"John",
+	"lastName":"Doe"
+}
+```  
+  
+Sample response  
+  
+  ```
+  {
+    "endpoint": "/hello",
+    "message": "Hello world, how are you?",
+    "payload": {
+        "firstName": "John",
+        "lastName": "Doe"
+    }
+}
+```
+  
+Method: GET/DELETE  
+
+``` 
+{
+    "endpoint": "/hello",
+    "message": "Hello world, how are you?"
+}
+```
+
+### Say Goodbye
+Endpoint: `<host>`:`<port>`/`<someInvalidEndpoint>`  
+Method: GET/POST/PUT/DELETE
+  
+
+Sample response  
+  
+```
+{
+    "error": "Resource '/someInvalidEndpoint' not found, goodbye."
+}
+```
--- index.js
+++ index.js
@@ -5,56 +5,122 @@
 const http = require('http');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
+const cluster = require('cluster');
+const os = require('os');
 
-const server = http.createServer((req, res) => {
-    handleRequest(req, res);
-})
 
-server.listen(3000, () => {
-    console.log('Listening on port 3000');
-})
+const HelloWorldApi = class HelloWorldApi {
+    /*
+    * Spawns worker processes across all cores to handle requests
+    */
+    static init() {
+        if(cluster.isMaster) {
+            // fork the process
+            os.cpus().forEach((cpu) => {
+                cluster.fork();
+            });
 
-const handleRequest = (req, res) => {
-    let parseUrl = url.parse(req.url, true);
-    let path = parseUrl.pathname;
-    let trimmedPath = path.replace(/^\/+|\/+$/g, '');
-    let decoder = new StringDecoder('utf-8');
-    let buffer = '';
-    req.on('data', (data) => {
-        buffer += decoder.write(data);
-    });
+        } else {
+            // if we're not in the master thread, start the HTTP server
+            ApiHandler.init(3000);
+        }
+    }
+}
 
-    req.on('end', () => {
-        buffer += decoder.end();
-        let chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handler.goodbye;
-        chosenHandler((statusCode) => {
-            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-            let data = {
-                resource : trimmedPath,
-                payload : buffer
-            }
-            res.setHeader('Content-Type', 'application/json')
-            if(statusCode == 200){
-                data.message = 'Hello world, thank you for posting';
-            } else {
-                data.message = 'Resource not found, goodbye';
-                delete data.payload;
-                res.statusCode = statusCode;
-            }
-            res.end(JSON.stringify(data));
+const ApiHandler = class ApiHandler {
+
+    /*
+    * Sets up server for requests
+    * 
+    * @param {port} port to listen for requests
+    * 
+    */
+    static init(port) {
+        this.router = new Map([['hello', this.sayHello]]);
+
+        let server = http.createServer((req, res) => {
+            this.handleRequest(req, res);
         });
-    });
-};
 
-const handler = {
-    hello : (callback) => {
+        server.listen(port, () => {
+            console.log(Colors.YELLOW, `The server is listening on port ${port}`);
+        });
+    }
+
+    /*
+    * Handles requests, parses payload accordingly
+    * 
+    * @param {req} HTTP request object
+    * @param {res} HTTP response object
+    * 
+    */
+    static handleRequest(req, res) {
+        let parsedUrl = url.parse(req.url, true);
+        let path = parsedUrl.pathname;
+        let trimmedPath = path.replace(/^\/+|\/+$/g, '');
+        let decoder = new StringDecoder('utf-8');
+        let method = req.method.toUpperCase();
+        let buffer = '';
+        req.on('data', (data) => { buffer += decoder.write(data); });
+
+        req.on('end', () => {
+            buffer += decoder.end();
+            let chosenHandler = typeof(this.router.get(trimmedPath)) !== 'undefined' ? this.router.get(trimmedPath): this.notFound;
+            chosenHandler((statusCode) => {
+                statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
+                let data = { 'endpoint' : path }
+                
+                res.setHeader('Content-Type', 'application/json')
+                if(statusCode === 200){
+                    data.message = 'Hello world, how are you?';
+                } else {
+                    data = { 'error' : `Resource '${path}' not found, goodbye.` }
+                    res.statusCode = statusCode;
+                }
+
+                if(buffer.length > 0  && method && (method === 'POST' || method === 'PUT')) {
+                    let payload;
+                    try {
+                        data.payload = JSON.parse(buffer);
+                    } catch(err) {
+                        data.error = 'Payload has to be in well-formed JSON format';
+                    }
+                }
+
+                res.end(JSON.stringify(data));
+            });
+        });
+    }
+
+    /*
+    * Simply invokes a 'success' callback, main handler for this demo app
+    * 
+    * @param {callback} HTTP response object
+    * 
+    */
+    static sayHello(callback) {
         callback(200);
-    },
-    goodbye : (callback) => {
+    }
+
+    /*
+    * Simply invokes a 'not found' callback, default handler for invalid endpoints
+    * 
+    * @param {callback} HTTP response object
+    * 
+    */
+    static notFound(callback) {
         callback(404);
     }
 }
 
-const router = {
-    hello : handler.hello
-};
+// Console output colors
+const Colors =  {
+    RED : '\x1b[31m%s\x1b[0m',
+    GREEN : '\x1b[32m%s\x1b[0m',
+    YELLOW : '\x1b[33m%s\x1b[0m',
+    DARK_BLUE: '\x1b[34m%s\x1b[0m',
+    MAGENTA : '\x1b[35m%s\x1b[0m',
+    LIGHT_BLUE: '\x1b[36m%s\x1b[0m'
+}
+
+HelloWorldApi.init();
